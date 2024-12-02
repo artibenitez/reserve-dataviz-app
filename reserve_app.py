3334897e4f2182304e8ca20029d662f7c7d104b0
Arturo Benitez <arti@Mac.lan>
2024-12-01 22:32:58 -0500

    updating comment

--- reserve_app.py
+++ reserve_app.py
@@ -6,14 +6,12 @@ import os
 def main():
     st.title("Heatmap Visualization")
 
-    # Path to the CSV file in the repository
-    csv_path = "data/balthazar_data.csv"  # Adjust path as needed
-
+    # Path to the CSV file in the repo
+    csv_path = "data/balthazar_data.csv"
     if not os.path.exists(csv_path):
         st.error(f"CSV file not found at {csv_path}. Please check the path and try again.")
         return
-
-    # Load the data
+    
     df = pd.read_csv(csv_path)
 
     # Ensure required columns exist
@@ -42,8 +40,7 @@ def main():
 
     filtered_df = df[df['table_size'].isin(table_size_filter)]
 
-    heatmap = filtered_df.groupby(['day_of_week', 'table_slot']).sum().iloc[:, [-1]]  # Use the Value column
-    heatmap.reset_index(inplace=True)
+    heatmap = filtered_df.groupby(['day_of_week', 'table_slot'])['Value'].sum().reset_index()
 
     day_mapper = {1: 'Sunday', 2: 'Monday', 3: 'Tuesday', 4: 'Wednesday', 5: 'Thursday', 6: 'Friday', 7: 'Saturday'}
     heatmap['day_str'] = heatmap['day_of_week'].map(day_mapper)
@@ -57,7 +54,7 @@ def main():
     num_time_slots = heatmap['table_slot_str'].nunique()
 
     # Define the size of the squares
-    square_size = 40  # Adjust this value for size
+    square_size = 40
 
     # Set chart properties to create squares
     heatmap_chart = alt.Chart(heatmap, title="Odds of Getting a Table").mark_rect().encode(
@@ -65,7 +62,7 @@ def main():
         x=alt.X('table_slot_str:O', sort=alt.EncodingSortField(field="table_slot", order='ascending'), title='Time Slot'),
         color=alt.Color('Value:Q', title='Odds',scale=alt.Scale(
             domain=list(heatmap['Value'].quantile([0, 0.05, 0.5, 0.95, 1])),
-            range=['white', '#d3f9d8', '#95d79e', '#4ca672', 'forestgreen']  # Gradient shades of green
+            range=['white', '#d3f9d8', '#95d79e', '#4ca672', 'forestgreen']
         )),
         tooltip=[
             alt.Tooltip('day_str', title='Weekday'),
@@ -73,13 +70,12 @@ def main():
             alt.Tooltip('Value', title='Odds of Table Availability')
         ]
     ).properties(
-        width=square_size * num_time_slots,  # Width based on number of time slots
-        height=square_size * num_days          # Height based on number of days
+        width=square_size * num_time_slots,  
+        height=square_size * num_days          
     ).configure_view(
-        strokeWidth=0  # Optional: Remove borders around the chart
+        strokeWidth=0 
     )
 
-    # Display the chart
     st.altair_chart(heatmap_chart, use_container_width=True)
 if __name__ == "__main__":
     main()
